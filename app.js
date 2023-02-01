bb5633d6bc05bc579d6f36b92db6cb842f5af290
nicola00 <nicolajobs00@gmail.com>
2023-02-01 15:43:31 +0100

    FEAT function

---
+++ app.js
@@ -0,0 +1,14 @@
+// 1. Dichiarare i vari elementi del document
+ let kmElement = document.querySelector('#train input[type="email"]')
+ let scontoElement = document.querySelector('#train select[name="color"]')
+ let btnElement =document.querySelector('#train input [type="button"]')
+ console.log( kmElement, scontoElement, btnElement)
+    
+// 2. Calcolare i km che vuole percorre l'utente
+
+
+// 3. Collegare i vari elementi al button
+
+formBtnElement.addEventListener('click', function () {
+    
+})
